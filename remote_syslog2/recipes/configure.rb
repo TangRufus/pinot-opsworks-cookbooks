30e5cef985e5ba0891dee3bada507457d9b77122
Tang Rufus <tangrufus@gmail.com>
2015-03-30 23:27:10 +0800

    remote_syslog2::configure: Handle Immutable types

--- remote_syslog2/recipes/configure.rb
+++ remote_syslog2/recipes/configure.rb
@@ -1,5 +1,16 @@
 file node['remote_syslog2']['config_file'] do
-  content node['remote_syslog2']['config'].to_hash.to_yaml
+  # Convert attribute classes to plain old ruby objects
+  config = node['remote_syslog2']['config'] ? node['remote_syslog2']['config'].to_hash : {}
+  config.each do |k, v|
+    case v
+    when Chef::Node::ImmutableArray
+      config[k] = v.to_a
+    when Chef::Node::ImmutableMash
+      config[k] = v.to_hash
+    end
+  end
+
+  content YAML::dump(config)
   mode '0644'
   notifies :restart, 'service[remote_syslog2]', :delayed
 end
