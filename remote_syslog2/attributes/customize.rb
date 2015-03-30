849a14c5f12d9d9aa506d04bc36c1e157fdd5e77
Tang Rufus <tangrufus@gmail.com>
2015-03-30 16:42:44 +0800

    Fork & Improve trappar/remote_syslog2-cookbook
    remote_syslog2: Fix typo
    
    Fix: ImmutableArray
    
    Delete: rails::precompile
    
    undefined method [] for nil:NilClass
    
    Fix: syntax error
    
    testing
    
    Fix: .to_s
    
    Fix: .to_s
    
    include_attribute 'nginx'
    
    include_attribute 'nginx::nginx'
    
    include_attribute 'nginx::nginx'
    
    include_attribute 'nginx::nginx'
    
    .split(',')
    
    Fix
    
    to_hash
    
    .map{}to_hash.to_yaml
    
    no to_ medeth

---
+++ remote_syslog2/attributes/customize.rb
@@ -0,0 +1,17 @@
+# Overwrite this in your cookbook
+
+include_attribute 'deploy'
+include_attribute 'nginx::nginx'
+
+node[:deploy].each do |application, deploy|
+  rails_logs = "#{deploy[:deploy_to]}/shared/log/*.log"
+  nginx_logs = "#{node[:nginx][:log_dir].to_s}/*.log"
+
+  normal['remote_syslog2']['config']['files'] = "[#{rails_logs}, #{nginx_logs}]"
+end
+
+normal['remote_syslog2']['config']['exclude_files'] = "[#{node[:papertrail][:exclude_files]}]"
+normal['remote_syslog2']['config']['exclude_patterns'] = "[#{node[:papertrail][:exclude__patterns]}]"
+normal['remote_syslog2']['config']['hostname'] = node[:opsworks][:instance][:hostname]
+normal['remote_syslog2']['config']['destination']['host'] = node[:papertrail][:host]
+normal['remote_syslog2']['config']['destination']['port'] = node[:papertrail][:port]
