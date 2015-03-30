1066dce05b128734c4ee13d79b1a6d6ae0de94b3
Tang Rufus <tangrufus@gmail.com>
2015-03-30 23:52:11 +0800

    sidekiq: typo

--- remote_syslog2/attributes/customize.rb
+++ remote_syslog2/attributes/customize.rb
@@ -10,8 +10,8 @@ node[:deploy].each do |application, deploy|
   normal['remote_syslog2']['config']['files'] = [rails_logs, nginx_logs]
 end
 
-normal['remote_syslog2']['config']['exclude_files'] = node[:papertrail][:exclude_files]
-normal['remote_syslog2']['config']['exclude_patterns'] = node[:papertrail][:exclude__patterns]
+normal['remote_syslog2']['config']['exclude_files'] = node[:papertrail][:exclude_files].split(',')
+normal['remote_syslog2']['config']['exclude_patterns'] = node[:papertrail][:exclude_patterns].split(',')
 normal['remote_syslog2']['config']['hostname'] = node[:opsworks][:instance][:hostname]
 normal['remote_syslog2']['config']['destination']['host'] = node[:papertrail][:host]
 normal['remote_syslog2']['config']['destination']['port'] = node[:papertrail][:port]
