a6d638f5c0a61b1f213621a29209214e05faace5
Tang Rufus <tangrufus@gmail.com>
2015-04-01 17:53:11 +0800

    Add node[:papertrail][:logs]

--- remote_syslog2/attributes/customize.rb
+++ remote_syslog2/attributes/customize.rb
@@ -6,8 +6,10 @@ include_attribute 'nginx::nginx'
 node[:deploy].each do |application, deploy|
   rails_logs = "#{deploy[:deploy_to]}/shared/log/*.log"
   nginx_logs = "#{node[:nginx][:log_dir].to_s}/*.log"
+  newrelic_logs = '/var/log/newrelic/*.log'
+  logs = node[:papertrail][:logs]
 
-  normal['remote_syslog2']['config']['files'] = [rails_logs, nginx_logs]
+  normal['remote_syslog2']['config']['files'] = [rails_logs, nginx_logs, newrelic_logs, logs].reject!(&:nil?).reject!(&:empty?)
 end
 
 normal['remote_syslog2']['config']['exclude_files'] = node[:papertrail][:exclude_files].split(',')
